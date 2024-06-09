d78a8e550fcb0959ce6a96e1c4cef1ae1a476174
nizamiftahul <nizamiftahul@gmail.com>
2024-06-09 10:47:52 +0700

    fixing

--- README.md
+++ README.md
@@ -47,7 +47,6 @@ import {
   login,
   authOTPMiddleware,
   generateOTP,
-  db,
   logout,
   refreshToken,
   verifyOTP,
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 export * from "./src/auth/authMiddleware";
 export * from "./src/auth/controller";
-export * from "./src/db";
 export * from "bcrypt";
 export * from "./src/utils/excludeProperties";
